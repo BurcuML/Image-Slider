222670e2ace53abcf64024c04d65b968c87ffbe9
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2025-04-07 19:11:11 +0300

    Refactor image navigation logic and clean up CSS formatting

--- script.js
+++ script.js
@@ -8,15 +8,19 @@ let currentImg = 0;
 const totalImages = allImages.length;
 const totalExplanations = explanations.length;
 
+
 // Hide all images except the first one
 for (let i = 1; i < totalImages; i++) {
   allImages[i].style.display = "none";
   explanations[i].style.display = "none";
 }
 
+//Neden % images.length Yapıyoruz?
+// Çünkü toplamda kaç tane resim varsa o kadar döngü yapması lazım.
+// Eğer 5 tane resim varsa 0-4 arasında döngü yapar. Eğer 6. resme geçerse 0'a döner.
+// Yani 5'e bölünce kalan 0 olur. Bu yüzden % kullanıyoruz.
+//Eğer % kullanmasaydık, son resmi geçince hata verirdi veya undefined olurdu.
 prev.addEventListener("click", () => {
-    // Scroll to the left by the width of the images container
-  images.scrollLeft -= images.offsetWidth;
   currentImg = (currentImg - 1 + totalImages) % totalImages;
 
   allImages.forEach((img, index) => {
@@ -25,9 +29,9 @@ prev.addEventListener("click", () => {
   });
 });
 
+
 next.addEventListener("click", () => {
-    // Scroll to the right by the width of the images container
-  images.scrollLeft += images.offsetWidth;
+
   currentImg = (currentImg + 1) % totalImages;
 
   allImages.forEach((img, index) => {
